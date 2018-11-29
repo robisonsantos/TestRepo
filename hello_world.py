72ea9c05ea5f172cd4ba01a63f882e2d7f919d9d
robison <rwrsantos@gmail.com>
2018-11-28 18:37:37 -0800

    Fixed bug and made it more user friendly

--- hello_world.py
+++ hello_world.py
@@ -1,8 +1,9 @@
 import sys
 
 def main():
-    name = sys.argv[0]
-    print "Hello {}".format(name)
+    name = sys.argv[2]
+    greeting = sys.argv[1]
+    print "{} {}".format(greeting, name)
 
 #---
 main()
